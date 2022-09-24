8148dfba430563c4f0a290b5981f22a107260b52
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-09-24 13:06:59 +0300

    Second

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+import Notiflix from 'notiflix';
+
+
+const formElement = document.querySelector('search-form');
+const galleryElement = document.querySelector('gallery');
+
+function search(event) {
+
+}
+
+
+
+formElement.addEventListener('submit', search);
